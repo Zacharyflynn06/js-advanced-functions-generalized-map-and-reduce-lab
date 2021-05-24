15c59dec63be31fb2934090e23decf893ce959b9
Zac <zacharyflynn06@gmail.com>
2021-05-24 13:15:35 -0600

    commit tree 180d10cc6848af27474dc499647d7045c3848b96

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+const map = (sourceArray, customFunction) => {
+    const newArray = []
+    for (let i=0; i < sourceArray.length; i++) {
+        newArray.push(customFunction(sourceArray[i]))
+    }
+
+    return newArray
+}
+
+const reduce = (sourceArray, customFunction, initialValue) => {
+    let total
+    if (initialValue) {
+        total = initialValue
+        for (let i = 0; i < sourceArray.length; i++) {
+            total = customFunction(sourceArray[i], total)
+        }
+        return total
+    } else {
+        total = sourceArray[0]
+        for (let i = 1; i < sourceArray.length; i++) {
+            total = customFunction(sourceArray[i], total)
+        }
+        return total
+    }
+}
